6ee5cab21356d21b27d2b7852b3c6c344cf6f50c
primitive12 <oueslatiahmed22@gmail.com>
2023-02-16 16:05:04 -0800

    commentaire

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,6 @@ printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-    return (0);
+return (0);
 }
 
