7621483fe3b6e3408dfa750fbb0b69fbf20fef75
primitive12 <oueslatiahmed22@gmail.com>
2023-02-17 16:46:31 -0800

    commentaire

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,24 @@
 
 int main(void)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < 9; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
 
-            if (i < 8)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+if (i < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+    
+putchar('\n');
 
-    putchar('\n');
-
-    return (0);
+return (0);
 }
