d66a0d7bd988858a1322bc58fbec12f29df625d5
primitive12 <oueslatiahmed22@gmail.com>
2023-02-17 16:36:55 -0800

    commentaire

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,18 @@
-#include <stdio.h>
-
-int main(void) {
+int main() {
     int i, j;
-
-    for (i = 0; i < 9; i++) {
+    for (i = 0; i <= 9; i++) {
         for (j = i + 1; j <= 9; j++) {
-            if (i == 0 && j == 9) {
-                continue; // skip "09"
-            }
-            putchar(i + '0');
-            putchar(j + '0');
-
-            if (i < 8) {
-                putchar(',');
-                putchar(' ');
+            int a = i == 0 ? 10 : i;
+            int b = j == 0 ? 10 : j;
+            if (a < b) {
+                putchar(i + '0');
+                putchar(j + '0');
+                if (i < 8) {
+                    putchar(',');
+                    putchar(' ');
+                }
             }
         }
     }
-
-    putchar('\n'); // print newline after the last pair of digits
-
     return 0;
 }
