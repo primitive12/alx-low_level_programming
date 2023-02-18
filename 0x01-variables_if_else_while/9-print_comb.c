23ee597ab565a21736f731681667d6d76cfa96aa
primitive12 <oueslatiahmed22@gmail.com>
2023-02-17 16:17:12 -0800

    commentaire

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
