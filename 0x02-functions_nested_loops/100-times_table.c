6a9a7dd4f898b7afc80c9cf61eac1ff088feecf4
primitive12 <oueslatiahmed22@gmail.com>
2023-02-21 16:28:00 -0800

    commentaire

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,67 +1,47 @@
-#include <stdio.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * print_times_table - function declaration
- * Description: print the time table
- * @n: the number to use
- * Return: void
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
-
-void print_times_table(int n);
-
-/**
- * print_times_table - function definition
- * Description: print entire times table of specified number
- * @n: number to use
- * Return: void
- */
-
-
 void print_times_table(int n)
 {
-	int row;
-	int column;
+	int i, j, k;
 
-	if (n >= 0 && n <= 20)
+	if (n >= 0 && n <= 15)
 	{
-		for (row = 0; row <= n; row++)
+		for (i = 0; i <= n; i++)
 		{
-			for (column = 0; column <= n; column++)
+			for (j = 0; j <= n; j++)
 			{
-				int product = column * row;
-
-				if (column == 0)
+				k = j * i;
+				if (j == 0)
 				{
-					putchar('0');
-				}
-				else if (product <= 9)
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar(' ');
-					putchar('0' + product);
-				}
-				else if (product > 99)
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
-					putchar(',');
-					putchar(' ');
-					putchar('0' + (product / 100));
-					putchar('0' + ((product / 10) % 10));
-					putchar('0' + (product % 10));
-				}
-				else
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
 				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar('0' + (product / 10));
-					putchar('0' + (product % 10));
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
