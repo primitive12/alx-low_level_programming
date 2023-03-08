d63d840631c4c6fa45d40ae66a3eb9ba0e65aea0
primitive12 <oueslatiahmed22@gmail.com>
2023-03-08 08:08:01 -0800

    commentaire

---
+++ 0x01-variables_if_else_while/10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{	
+	 int a =48;
+ 	 while (a < 58)
+{	
+		 putchar(a);
+		 a++;
+}
+	 putchar(10);
+	 return(0);
+}
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{	
-	 int a =48;
- 	 while (a < 58)
-{	
-		 putchar(a);
-		 a++;
-}
-	 putchar(10);
-	 return(0);
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
 }
